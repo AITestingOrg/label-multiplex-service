9321280b51b49fdb270de38c6597f64a69855ec9
Justin Phillips <euangelion3o9@gmail.com>
2018-05-21 17:21:53 -0400

    Initial code commit

---
+++ src/main/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplication.java
@@ -0,0 +1,12 @@
+package org.aist.aide.labelmultiplexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelMultiplexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelMultiplexerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.aist.aide.labelmultiplexer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabelMultiplexerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
