ab6ddfe89d8ca7338ef1fcbcd2c45371c6da2b95
Justin Phillips <euangelion3o9@gmail.com>
2018-05-31 14:28:11 -0400

    Added Histrix to project.

--- src/main/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplication.java
+++ src/main/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplication.java
@@ -2,10 +2,12 @@ package org.aist.aide.labelmultiplexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableJpaAuditing
 @EnableDiscoveryClient
 public class LabelMultiplexerApplication {
