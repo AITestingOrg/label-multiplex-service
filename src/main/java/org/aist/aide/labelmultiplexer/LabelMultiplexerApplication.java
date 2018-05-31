f31e29036ec37eff2432573d48fdf69f7c960e49
Justin Phillips <euangelion3o9@gmail.com>
2018-05-31 11:37:30 -0400

    Added Eureka registration.

--- src/main/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplication.java
+++ src/main/java/org/aist/aide/labelmultiplexer/LabelMultiplexerApplication.java
@@ -2,10 +2,12 @@ package org.aist.aide.labelmultiplexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class LabelMultiplexerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabelMultiplexerApplication.class, args);
